f3f412cd4e9c4598c7ba48f571487f36ef234d26
wrkstrm <cmonterroza@gmail.com>
2013-04-04 15:12:31 -0400

    A category for NSObjects to time themselves at the Nanosecond scale.

--- WSNanoTimer/AppDelegate.m
+++ WSNanoTimer/AppDelegate.m
@@ -10,12 +10,25 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    CFTimeInterval executionTimeMilli = [self executionTime: kWSTimeUnitMillisecond block:^{
+        NSLog(@"You can add or subtract between 0 to 9 decimal places of accuracy.");
+        NSLog(@"With the \"kWSTimeUnitMillisecond\" option 16.667 = 1/60 (1 frame)");
+    }];
+    NSLog(@"- Result: The previous 2 logs took this many milliseconds to complete: %f", executionTimeMilli);
+    
+    CFTimeInterval executionTimeMilliPlus1 = [self executionTime: kWSTimeUnitMillisecond + 1 block:^{
+        NSLog(@"With the \"kWSTimeUnitMillisecond + 1\" option 166.6667 = 1/60 (1 frame)");
+        NSLog(@"This resolution is useful because it makes the cost of individual calls visible.");
+    }];
+    NSLog(@"- Result: The previous log took this many 1/10,000th of seconds to complete: %f", executionTimeMilliPlus1);
+    
+    CFTimeInterval executionTimeNano = [self executionTime: kWSTimeUnitNanosecond block:^{}];
+    NSLog(@"- Result: I guess it takes this many Nanoseconds to call an empty block: %f", executionTimeNano);
     // Override point for customization after application launch.
     return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -24,7 +37,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
---
+++ WSNanoTimer/NSObject+WSNanoTimer.m
@@ -0,0 +1,36 @@
+//
+//  NSObject+WSNanoTimer.m
+//  WSNanoTimer
+//
+//  Created by Cristian A Monterroza on 4/4/13.
+//  Copyright (c) 2013 wrkstrm. All rights reserved.
+//
+
+#import "NSObject+WSNanoTimer.h"
+#import <mach/mach_time.h>
+
+static uint64_t sTimebaseRatio;
+
+@implementation NSObject (WSNanoTimer)
+
++ (void) load {
+    mach_timebase_info_data_t sTimebaseInfo;
+    
+    mach_timebase_info(&sTimebaseInfo);
+    
+    sTimebaseRatio = sTimebaseInfo.numer / sTimebaseInfo.denom;
+}
+
+-(CFTimeInterval) executionTime: (WSTimeUnit) timeUnit block: (void (^)(void)) block {
+    
+    NSUInteger startTime = mach_absolute_time();
+    
+    block();
+    
+    NSUInteger endTime = mach_absolute_time();
+    
+    NSLog(@"Exponent: %f", pow(10, 9 - timeUnit));
+    return (endTime - startTime) * sTimebaseRatio / pow(10, 9 - timeUnit);
+}
+
+@end
